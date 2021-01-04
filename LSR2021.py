00b5aef73eb4c30329ebd79b9be77c5fb8514b5a
Brenna Connolly <connolly.brenna@gmail.com>
2021-01-04 16:38:54 -0500

    Add HTML backups of Bills

--- LSR2021.py
+++ LSR2021.py
@@ -1,10 +1,22 @@
 import pandas as pd
 import requests
-df = pd.read_csv (r'C:\scripts\2021-Legislation\2021_LSR.csv')
+from bs4 import BeautifulSoup
+import os
+
+df = pd.read_csv (r'C:\git\2021-Legislation\2021_LSR.csv')
 for index, row in df.iterrows():
     num = int(row.LSRNumber.split('-')[1])
     payload = {'id': num, 'type': '4'}
     r = requests.get('http://www.gencourt.state.nh.us/lsr_search/billText.aspx', params=payload)
     if r.text != 'error: Index 0 is either negative or above rows count.':
         row.BillURL = r.url
-df.to_csv(r'C:\scripts\2021-Legislation\2021_LSR.csv', index = False)
+        soup = BeautifulSoup(r.content, features="lxml")
+        txt = soup.get_text()
+        with open((r'C:\git\2021-Legislation\Bills-HTML' + '\\' + row.LSRNumber + ".html"), "w", encoding="utf-8") as f:
+            if(txt != 'error: Index 0 is either negative or above rows count.\r\n\r\n'):
+                f.write(str(soup))
+                f.close()
+            else:
+                f.close()
+                os.remove(r'C:\git\2021-Legislation\Bills-HTML' + '\\' + row.LSRNumber + ".html")
+df.to_csv(r'C:\git\2021-Legislation\2021_LSR.csv', index = False)
